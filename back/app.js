8351fc94b53d17da490e4a95ae2cbb04674c4f5e
RiinaPiiroinen <55059598+RiinaPiiroinen@users.noreply.github.com>
2019-10-04 11:00:12 +0300

    Update app.js

--- back/app.js
+++ back/app.js
@@ -13,13 +13,16 @@ app.use(function(req, res, next) {
     next();
     });
 
+
 const userRoutes = require('./routes/user');
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+app.use(cors());
 
 app.use('/user', userRoutes);
+
 
 app.use((req, res, next) => {
     const error = new Error('Not found');
